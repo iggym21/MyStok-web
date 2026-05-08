18360b35f1e6d9958bbe4014ddcb0f17a967b681
iggym21 <ignatiusmartin6@gmail.com>
2026-05-08 12:42:03 -0400

    feat: wire up React Router routes in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout.jsx'
+import InputPage from './pages/InputPage.jsx'
+import ResultsPage from './pages/ResultsPage.jsx'
+import DetailPage from './pages/DetailPage.jsx'
 
 export default function App() {
-  return <Layout><p className="text-white">Hello</p></Layout>
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/"               element={<InputPage />} />
+        <Route path="/results"        element={<ResultsPage />} />
+        <Route path="/stocks/:ticker" element={<DetailPage />} />
+      </Routes>
+    </Layout>
+  )
 }
